7a7abdeea0b5406ef3c8354c5f99310c0adefe0a
Zacharia <ndunguzachary24@gmail.com>
2022-06-08 15:39:08 +0300

    commit tree 2d199190174f6d9a7ec5890af192a4a6a4ec906c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun( prim = "roller-skate" ){
+ return `This Saturday, I want to ${prim}!`
+}
+const mondayWork = function(val = "go to the office"){
+    return `This Monday, I will ${val}.`
+}
+const wrapAdjective = function(pemm="*" ) {
+    return function( ){
+return `You are ${pemm}a hard worker${pemm}!`
+}  
+}
+
